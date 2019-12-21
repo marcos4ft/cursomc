26e9d5a0a2eebb700ccb719c5e81b68db6ded163
QUEIROZ Marcos <mqueiroz@europe.corp.altran.com>
2019-12-21 16:31:10 +0000

    Autorizando endpoints para perfis específicos

--- src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,7 @@ import com.nelioalves.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -39,6 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHES_GET = { "/h2-console/**", "/produtos/**", "/categorias/**" };
 
+	private static final String[] PUBLIC_MATCHES_POST = { "/clientes/**" };
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
@@ -47,8 +51,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		}
 
 		http.cors().and().csrf().disable();
-		http.authorizeRequests().antMatchers(PUBLIC_MATCHES).permitAll().antMatchers(HttpMethod.GET, PUBLIC_MATCHES_GET)
-				.permitAll().anyRequest().authenticated();
+		http.authorizeRequests()
+		.antMatchers(PUBLIC_MATCHES).permitAll()
+		.antMatchers(HttpMethod.POST, PUBLIC_MATCHES_POST).permitAll()
+		.antMatchers(HttpMethod.GET, PUBLIC_MATCHES_GET).permitAll()
+		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
--- src/main/java/com/nelioalves/cursomc/resources/ClienteResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/ClienteResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,12 +44,14 @@ public class ClienteResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<ClienteDTO>> findAll() {
 		List<Cliente> list = service.findAll();
@@ -56,9 +59,9 @@ public class ClienteResource {
 		return ResponseEntity.ok(listDTO);
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
-	public ResponseEntity<Page<ClienteDTO>> findPage(
-			@RequestParam(name = "page", defaultValue = "0") Integer page,
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(name = "page", defaultValue = "0") Integer page,
 			@RequestParam(name = "linePerPage", defaultValue = "24") Integer linePerPage,
 			@RequestParam(name = "orderBy", defaultValue = "nome") String orderBy,
 			@RequestParam(name = "direction", defaultValue = "ASC") String direction) {
